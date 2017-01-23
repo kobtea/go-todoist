5871048d8657383160261f6018b88832452a8626
kobtea <kobtea9696@gmail.com>
2017-01-24 00:20:28 +0900

    implement project methods

--- todoist/project.go
+++ todoist/project.go
@@ -1,5 +1,10 @@
 package todoist
 
+import (
+	"errors"
+	"fmt"
+)
+
 type Project struct {
 	Entity
 	Name         string `json:"name"`
@@ -11,4 +16,73 @@ type Project struct {
 	IsArchived   int    `json:"is_archived"`
 	InboxProject bool   `json:"inbox_project"`
 	TeamInbox    bool   `json:"team_inbox"`
+}
+
+type ProjectManager struct {
+	*Client
+}
+
+func (m *ProjectManager) Add(project Project) (*Project, error) {
+	if len(project.Name) == 0 {
+		return nil, errors.New("New project requires a name")
+	}
+	project.ID = GenerateTempID()
+	m.SyncState.Projects = append(m.SyncState.Projects, project)
+	command := Command{
+		Type:   "project_add",
+		Args:   project,
+		UUID:   GenerateUUID(),
+		TempID: project.ID,
+	}
+	m.queue = append(m.queue, command)
+	return &project, nil
+}
+
+func (m *ProjectManager) Update(project Project) (*Project, error) {
+	if !IsValidID(project.ID) {
+		return nil, fmt.Errorf("Invalid id: %s", project.ID)
+	}
+	command := Command{
+		Type: "project_update",
+		Args: project,
+		UUID: GenerateUUID(),
+	}
+	m.queue = append(m.queue, command)
+	return &project, nil
+}
+
+func (m *ProjectManager) Delete(ids []ID) error {
+	command := Command{
+		Type: "project_delete",
+		UUID: GenerateUUID(),
+		Args: map[string][]ID{
+			"ids": ids,
+		},
+	}
+	m.queue = append(m.queue, command)
+	return nil
+}
+
+func (m *ProjectManager) Archive(ids []ID) error {
+	command := Command{
+		Type: "project_archive",
+		UUID: GenerateUUID(),
+		Args: map[string][]ID{
+			"ids": ids,
+		},
+	}
+	m.queue = append(m.queue, command)
+	return nil
+}
+
+func (m *ProjectManager) Unarchive(ids []ID) error {
+	command := Command{
+		Type: "project_unarchive",
+		UUID: GenerateUUID(),
+		Args: map[string][]ID{
+			"ids": ids,
+		},
+	}
+	m.queue = append(m.queue, command)
+	return nil
 }
